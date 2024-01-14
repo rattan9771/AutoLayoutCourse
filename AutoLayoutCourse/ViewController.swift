59410e4f78cba7b539c304e2196b2b663b50ebc1
Rattan <rattan@cqlsys.co.uk>
2024-01-14 23:04:01 +0530

    Initial Commit

---
+++ AutoLayoutCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutCourse
+//
+//  Created by Rattan Das on 14/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
