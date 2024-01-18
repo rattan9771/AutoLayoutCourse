2cc6a8d951ef1e2dc21c8971f4801bccaa5513de
Rattan <rattan@cqlsys.co.uk>
2024-01-18 18:03:26 +0530

    programatic constraints done

--- AutoLayoutCourse/ViewController.swift
+++ AutoLayoutCourse/ViewController.swift
@@ -10,38 +10,123 @@ import UIKit
 class PriorityTestViewController: UIViewController {
 
    
-    @IBOutlet weak var leadingYellow: NSLayoutConstraint!
     
-    @IBOutlet weak var bottom: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .yellow
         
-      
+       let lbl = addLabel()
+       let bt1 = addBtn1(lbl : lbl)
+        
+        
+        let v1 = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 36, height: 0))
+        v1.backgroundColor = .green
+        v1.layer.borderColor = UIColor.lightGray.cgColor
+        v1.layer.borderWidth = 2
+        
+        v1.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(v1)
+        
+        NSLayoutConstraint(item: v1, attribute: .top, relatedBy: .equal, toItem: bt1, attribute: .bottom, multiplier: 1.0, constant: 24).isActive = true
+        
+        NSLayoutConstraint(item: v1, attribute: .bottom, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: -8).isActive = true
+
+        NSLayoutConstraint(item: v1, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 18).isActive = true
+
+       NSLayoutConstraint(item: v1, attribute: .trailing, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: -18).isActive = true
+        
+        let bt2 = addBtn2(v1 : v1)
+        
+        let bt3 = addBtn3(v1 : v1, bt2 : bt2)
+    }
+    
+    
+    func addBtn3(v1 : UIView, bt2 : UIButton) -> UIButton {
+        let bt1 = UIButton()
+        bt1.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        bt1.setTitle("Button 3", for: .normal)
+        bt1.setTitleColor(UIColor.red, for: .normal)
+        bt1.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        bt1.backgroundColor = UIColor.blue
+        
+        v1.addSubview(bt1)
+        bt1.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: bt1, attribute: .top, relatedBy: .equal, toItem: bt2, attribute: .bottom, multiplier: 1.0, constant: 24).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .centerX, relatedBy: .equal, toItem: v1, attribute: .centerX, multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .width, relatedBy: .equal, toItem: v1, attribute: .width, multiplier: 0.5, constant: 0).isActive = true
+        
+        return bt1
+    }
+    
+    func addBtn2(v1 : UIView) -> UIButton {
+        let bt1 = UIButton()
+        bt1.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        bt1.setTitle("Button 2", for: .normal)
+        bt1.setTitleColor(UIColor.red, for: .normal)
+        bt1.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        bt1.backgroundColor = UIColor.blue
+        
+        v1.addSubview(bt1)
+        bt1.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: bt1, attribute: .top, relatedBy: .equal, toItem: v1, attribute: .top, multiplier: 1.0, constant: 24).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .centerX, relatedBy: .equal, toItem: v1, attribute: .centerX, multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .width, relatedBy: .equal, toItem: v1, attribute: .width, multiplier: 0.5, constant: 0).isActive = true
+        
+        return bt1
+    }
+    
+    func addBtn1<T>(lbl: T) -> UIButton {
+        let bt1 = UIButton()
+        bt1.frame = CGRect(x: 0, y: 0, width: self.view.frame.width - 36, height: 40)
+        bt1.setTitle("Button 1", for: .normal)
+        bt1.setTitleColor(UIColor.red, for: .normal)
+        bt1.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        bt1.backgroundColor = UIColor.blue
+        
+        self.view.addSubview(bt1)
+        bt1.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: bt1, attribute: .top, relatedBy: .equal, toItem: lbl, attribute: .bottom, multiplier: 1.0, constant: 24).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 18).isActive = true
+        
+        NSLayoutConstraint(item: bt1, attribute: .trailing, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: -18).isActive = true
+        
+        return bt1
+    }
+    
+    func addLabel() -> UILabel {
+        
+        let titleLabel = UILabel()
+        titleLabel.frame = CGRect(x: 0, y: 0, width: self.view.frame.width - 36, height: 40)
+        titleLabel.textColor = .red
+        titleLabel.text = "Programatic Constraint"
+        titleLabel.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(titleLabel)
+        
+        
+        
+        NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .top, multiplier: 1.0, constant: 24).isActive = true
+        
+        NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1.0, constant: 18).isActive = true
+        
+        NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1.0, constant: -18).isActive = true
+        
+        return titleLabel
     }
 
-    @IBAction func tapMe(_ sender: Any) {
-        
-        UIView.animate(withDuration: 3.0) {
-            
-            if self.leadingYellow.constant == 0 {
-                self.leadingYellow.constant = -(self.view.frame.width)
-            }else {
-                self.leadingYellow.constant = 0
-            }
-            
-            if self.bottom.constant == -500 {
-                self.bottom.constant = 0
-            }else {
-                self.bottom.constant = -500
-            }
-            
-            self.view.layoutIfNeeded()
-            
-        }
-        
-        
-    }
+   
     
 }
 
