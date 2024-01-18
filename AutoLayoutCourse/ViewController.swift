6475bd255755d91d04727757c5a97f48dee0b8d3
Rattan <rattan@cqlsys.co.uk>
2024-01-18 13:58:31 +0530

    auto layout done

--- AutoLayoutCourse/ViewController.swift
+++ AutoLayoutCourse/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PriorityTestViewController: UIViewController {
 
+    @IBOutlet weak var playbackbtn: UIButton!
+    
+    @IBOutlet weak var ibookBtn: UIButton!
+    
+    @IBOutlet weak var kindleBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapBtn(sender : UIButton) {
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            switch sender.tag {
+                
+            case 0:
+                self.playbackbtn.removeFromSuperview()
+                
+            case 1:
+                self.ibookBtn.removeFromSuperview()
+                
+            case 2:
+                self.kindleBtn.removeFromSuperview()
+                
+            default:
+                print("nothing")
+            }
+            
+            self.view.layoutIfNeeded()
+        }
+       
+        
+        
+    }
 
 }
 
